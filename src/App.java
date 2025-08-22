0cc2946c864a64f9c6b330ca99eb412923ba4410
Fdanna123 <maria_danna@estudante.sesisenai.org.br>
2025-08-22 16:15:34 -0300

    Commit_1
    Primeiro: Código inicial do projeto

--- src/App.java
+++ src/App.java
@@ -1,5 +1,25 @@
 public class App {
+    public static float n1, n2, n3, mf;
+    public static String status;
+
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        n1 = 5.6f;
+        n2 = 5.8f;
+        n3 = 4.9f;
+
+        // cálculo da média
+        mf = (n1 + n2 + n3) / 3;
+
+        // verificando situação
+        if (mf >= 7) {
+            System.out.println("Aluno aprovado ✅");
+        } else if (mf >= 5) {
+            System.out.println("Aluno em recuperação ⚠️");
+        } else {
+            System.out.println("Aluno reprovado ❌");
+        }
+
+        // mostrando a média também
+        System.out.println("Média final: " + mf);
     }
 }
